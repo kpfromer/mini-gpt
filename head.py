fix: bad softmax

head.py
@@ -80,7 +80,7 @@ class Head(nn.Module):
         wei = q @ k.transpose(
             -2, -1) * C**-0.5  # (B, T, C) @ (B, C, T) -> (B, T, T)
         wei = wei.masked_fill(self.tril[:T, :T] == 0, float('-inf'))
-        wei = F.softmax(wei, dim=1)
+        wei = F.softmax(wei, dim=-1)
         # perform the weighted aggreation of the values
         v = self.value(x)  # (B,T,C)
         out = wei @ v  # (B,T,T) @ (B,T,C) -> (B,T,C)